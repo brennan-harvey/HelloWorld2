c507f947ee99df1daeee70c2e6a68d440f566dfd
bharvey <brennan.harvey@maine.edu>
2019-01-30 13:56:01 -0500

    Added message to Charlie

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,18 @@ public class Main {
     	// Original message from lab 1
     	System.out.println("Hello World!");
     	
+    	/***** Merge conflict additions *****/
+    	
     	// Message to Alice on AliceBranch (lab2)
     	System.out.println("And hello Alice!");
     	
     	// Message to Bob on BobBranch (lab2)
     	System.out.println("And hi to Bob too!");
+    	
+    	/***** Additions done the right way *****/
+    	
+    	// Message to Charlie on CharlieBranch (lab2)
+    	System.out.println("Hey Charlie!");
     	
     	// Goodbye message from lab 2
     	System.out.println("See you later!");
