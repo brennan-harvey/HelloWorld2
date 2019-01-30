3a12633f79bf0ac65ed8a02d14ebb21e8e8f083a
bharvey <brennan.harvey@maine.edu>
2019-01-30 11:22:17 -0500

    Added preamble message for lab 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    	// Preamble message from lab 2
+    	System.out.println("Here ye : important announcement");
+    	
+    	// Original message from lab 1
+    	System.out.println("Hello World!");
     }
 }
